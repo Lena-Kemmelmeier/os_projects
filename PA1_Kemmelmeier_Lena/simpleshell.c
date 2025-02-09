c525d13f8b8a94f6f4930485cad1926c063f2aa9
Lena Kemmelmeier <97976019+Lena-Kemmelmeier@users.noreply.github.com>
2025-02-08 20:20:41 -0800

    working on parse input, little oopsie for now

--- PA1_Kemmelmeier_Lena/simpleshell.c
+++ PA1_Kemmelmeier_Lena/simpleshell.c
@@ -19,27 +19,31 @@ int executeCommand(char* const* enteredCommand, const char* infile, const char* 
 void changeDirectories(const char* path);
 
 // main function
-
 int main(){
 
     //for(;;){ // start of for loop, loop until the user chooses too - this is the example of an infinite loop from the class materials
 
         // initialize variables
-        int maxStringLength = 100; // arbitrary limit
+        int maxStringLength = 100; // arbitrary limit on the string
         char cwd[maxStringLength]; // statically allocated
+        char cli[maxStringLength]; // this is the input from user in CLI, statically allocated
         char *netID = "lkemmelmeier"; // my NetID, for the prompt
-        getcwd(cwd, maxStringLength); 
+        getcwd(cwd, maxStringLength); // gets the current working directory, this is stored in cwd
 
         // display the prompt to the user
         printf("%s:%s$", netID, cwd);
 
         // get input from the user
-        char input[maxStringLength];
-        fgets(input, maxStringLength, stdin);
+        fgets(cli, maxStringLength, stdin); // fgets allows us to take an input that has spaces in it
         //printf("%s",input); // check - are we getting the input correctly?
 
         // parse the input to the user
-        
+        char separatedInput[maxStringLength][500]; // allocate 2d static arr that will store the parsed strings
+        int validElements; // the valid number of cli elements
+        // here, 500 is the max number of strings/words (see function prototype we were given in the class materials)
+        validElements = parseInput(cli,separatedInput, maxStringLength); 
+
+        printf("num valid elements: %d/n",validElements);
         
     //}
     
@@ -47,3 +51,19 @@ int main(){
 }
 
 // function definitions
+int parseInput(char* input, char splitWords[][500], int maxWords){
+    char* word = NULL; // assumed delimeter is space here
+    int wordCount = 0;
+
+    // check - does strtok work?
+    do{
+        word = strtok(input," ");
+        printf("Word: %s/n", word); 
+        wordCount++;
+        
+
+    } while (word != NULL);
+
+    return wordCount;
+
+}
