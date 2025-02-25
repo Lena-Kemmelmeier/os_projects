d15105dd8e4aaaa810da1ce4d961bbd2723674ac
Lena Kemmelmeier <97976019+Lena-Kemmelmeier@users.noreply.github.com>
2025-02-24 22:12:44 -0800

    working on readFile

--- PA2_Kemmelmeier_Lena/threaded_sum.c
+++ PA2_Kemmelmeier_Lena/threaded_sum.c
@@ -15,6 +15,9 @@
 #include <pthread.h> // for pthreds
 #include <sys/time.h>
 
+// function prototypes
+int readFile(char fileName[], int intArr[]);
+
 // struct declaration
 typedef struct _thread_data_t {
     const int *data;
@@ -32,8 +35,31 @@ int main(int argc, char* argv[]){
         printf("%d\n",argc);
     }
     else{
-        
+
     }
 
     return 0;
+}
+
+// function definitions
+int readFile(char fileName[], int intArr[]){
+    int numItemsParsed = 0; // number of numbers in the file
+
+    // create input file stream and open it for reading
+    FILE* file = fopen(fileName,"r");
+
+    if(file == NULL){ // file does not exist
+        printf("File not found...\n");
+        return -1;
+    }
+    else{ // the file exists, read in the numbers
+
+
+        // as long as we are reading in at least one int, store into array, increment count
+        while(fscanf(file, "%d", &intArr[numItemsParsed]) > 0){
+            numItemsParsed++;
+        }
+
+        return numItemsParsed;
+    }
 }
