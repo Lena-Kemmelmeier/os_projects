99a18a404092a2e2d96f7f0f0d82d11269d6ce39
Lena Kemmelmeier <97976019+Lena-Kemmelmeier@users.noreply.github.com>
2025-02-27 12:32:00 -0800

    Maithy office hour edits - fixed seg fault!

--- PA2_Kemmelmeier_Lena/threaded_sum.c
+++ PA2_Kemmelmeier_Lena/threaded_sum.c
@@ -15,9 +15,11 @@
 #include <pthread.h> // for pthreads
 #include <sys/time.h>
 
+#define MAX_SIZE 100000000
+//#define MAX_SIZE 1000000
+
 // function prototypes
 int readFile(char fileName[], int intArr[]); // I am dynamically allocating memory due to a past seg fault issue
-//int readFile(char* fileName, int** intArr); // I am dynamically allocating memory due to a past seg fault issue
 void* arraySum(void* threadInputData);
 
 // struct declaration
@@ -43,11 +45,11 @@ int main(int argc, char* argv[]){
 
     // read in all data from the file that corresponds to the command-line-provided filename
     //int maxAmountNums = 100000000; // max number of numbers - as specified in assignment note
-    int maxAmountNums = 1000000; // max number of numbers - as specified in assignment note
-    int numbersArray[maxAmountNums];
+    // int maxAmountNums = 1000000; // max number of numbers - as specified in assignment note
+    int* numbersArray = malloc(MAX_SIZE*sizeof(int));
     //int* numbersArray;
-    //int numValuesRead = readFile(argv[1], &numbersArray); //argv[1] is the second argument aka fileNmae
     int numValuesRead = readFile(argv[1], numbersArray); //argv[1] is the second argument aka fileNmae
+    //int numValuesRead = readFile(argv[1], numbersArray); //argv[1] is the second argument aka fileNmae
 
 
     // printf("%d\n",numValuesRead);
@@ -55,14 +57,16 @@ int main(int argc, char* argv[]){
     // make sure number of threads requested is less than amount of values read
     // atoi converts argv[2] from a string to an int
     int numThreads = atoi(argv[2]); // argv[2] is the number of threads requested (third argument)
+    //printf("%d\n",numThreads); // check
+
     if(numThreads > numValuesRead){ 
         printf("Too many threads requested!\n");
         return -1;
     }
 
-    // check - is the array actually storing the ints from the file?
+    // //check - is the array actually storing the ints from the file?
     // for (int i = 0; i < numValuesRead; i++){
-    //     printf("%d\n",numbersArray[i]);
+    //      printf("%d\n",numbersArray[i]);
     // }
 
     long long int totalSum = 0; // keeps track of the sum, as specified in the directions
@@ -76,8 +80,8 @@ int main(int argc, char* argv[]){
     pthread_mutex_t lock; // created threads will use this for locking
     pthread_mutex_init(&lock, NULL); // NULL used in self-initializing routine example on die.net
 
-    // thread_data_t* threadDataArr = malloc(numThreads * sizeof(thread_data_t));
-    thread_data_t threadDataArr[numThreads];
+    thread_data_t* threadDataArr = malloc(numThreads * sizeof(thread_data_t));
+    //thread_data_t threadDataArr[numThreads];
 
     for (int i = 0; i < numThreads; i++){
         threadDataArr[i].data = numbersArray;
@@ -90,8 +94,8 @@ int main(int argc, char* argv[]){
     }
 
     // create all the threads
-    //pthread_t* threads = malloc(numThreads * sizeof(pthread_t));
-    pthread_t threads[numThreads];
+    pthread_t* threads = malloc(numThreads * sizeof(pthread_t));
+    //pthread_t threads[numThreads];
     for(int i = 0; i < numThreads; i++){
         pthread_create(&threads[i], NULL, arraySum, &threadDataArr[i]);
     }
@@ -112,9 +116,9 @@ int main(int argc, char* argv[]){
     printf("Final sum: %lld\n", totalSum);
 
     pthread_mutex_destroy(&lock); // destroy the lock
-    // free(numbersArray);
-    // free(threads);
-    // free(threadDataArr);
+    free(numbersArray);
+    free(threads);
+    free(threadDataArr);
     return 0;
 }
 
@@ -132,7 +136,7 @@ int readFile(char fileName[], int intArr[]){
     
     // the file exists, so read in the numbers
     // as long as we are reading in at least one int, store into array, increment count
-    while(fscanf(file, "%d", &intArr[numItemsParsed]) > 0){
+    while(fscanf(file, "%d", &intArr[numItemsParsed]) == 1){
         numItemsParsed++;
     }
 
